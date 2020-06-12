d0dedf451423cf5a173051476e99ce61064a6e8c
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2020-06-12 22:01:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,12 +2,16 @@ var database;
 
 var paint = [];
 var present = [];
+var position;
 
 function setup(){
    canvas = createCanvas(400,400);
    canvas.mousePressed(startDrawing);   
    
-   database = firebase.database();   
+   database = firebase.database();  
+   
+   var presentpos = database.ref('position');
+   presentpos.on("value",readData);
 }
 
 function startDrawing(){
@@ -21,13 +25,7 @@ function mousePressed(){
       x: mouseX,
       y: mouseY
    }
-   present.push(point);   
-}
-
-
-function draw(){
-   background(0);
-   
+   present.push(point);  
    stroke(255);
    strokeWeight(4);
    noFill();
@@ -38,8 +36,25 @@ function draw(){
          vertex(drawing[j].x,drawing[j].y);
       }
       endShape();
-   }
-   
+   } 
 }
+
+function draw(){
+   background(0); 
+}
+
+function recordPos(){
+   database.ref('position').set({
+      'x' : position.x + x,
+      'y' : position.y + y
+   });
+}
+
+function readData(data){
+   position = data.val();
+   paint.x = position.x;
+   paint.y = position.y;
+}
+
 
 
