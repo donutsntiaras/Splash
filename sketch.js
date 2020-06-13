4f12d8872f129162177c62a09388241491ec464e
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2020-06-13 20:14:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,60 +1,57 @@
 var database;
 
 var paint = [];
-var present = [];
+var drawing = [];
 var position;
 
 function setup(){
    canvas = createCanvas(400,400);
-   canvas.mousePressed(startDrawing);   
+   //canvas.mousePressed(startDrawing);   
    
    database = firebase.database();  
-   
-   var presentpos = database.ref('position');
-   presentpos.on("value",readData);
+      
 }
 
-function startDrawing(){
+/*function startDrawing(){
    present = [];
    paint.push(present);
-}
+}*/
 
 
-function mousePressed(){
+function mouseDragged(){
    var point = {
       x: mouseX,
       y: mouseY
    }
-   present.push(point);  
-   stroke(255);
-   strokeWeight(4);
-   noFill();
-   for(var i = 0; i> paint.length; i++){
-      var drawing = paint[i];
-      beginShape();
-      for(var j = 0; j> drawing.length; j++){
-         vertex(drawing[j].x,drawing[j].y);
-      }
-      endShape();
-   } 
+   paint.push(point);  
+   var dref = dtatabase.ref('drawing');
+   dref.set({
+      "d": paint
+   })   
 }
 
 function draw(){
    background(0); 
+   recordPos();
+   stroke(255);
+   strokeWeight(4);
+   noFill();
+   //beginShape();
+   for(var j = 0; j> drawing.length; j++){
+      vertex(drawing[j].x,drawing[j].y);
+      endShape();
+   }
+      
+    
 }
 
 function recordPos(){
-   database.ref('position').set({
-      'x' : position.x + x,
-      'y' : position.y + y
-   });
+   database.ref('drawing/').on('value', (data) => {
+      paint = data.val().d
+  })
 }
 
-function readData(data){
-   position = data.val();
-   paint.x = position.x;
-   paint.y = position.y;
-}
+
 
 
 
