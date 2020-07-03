0b36a537fb0d19ad39435591e0f3cd7c6d38bda7
Donuts_n_Tiaras <61537524+donutsntiaras@users.noreply.github.com>
2020-07-03 20:42:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,57 +1,69 @@
 var database;
 
-var paint = [];
-var drawing = [];
-var position;
+var myPaint = [];
+var currentPath = [];
+var isDrawing = false;
 
-function setup(){
-   canvas = createCanvas(400,400);
-   //canvas.mousePressed(startDrawing);   
-   
-   database = firebase.database();  
-      
+function setup() {
+    canvas = createCanvas(400, 400);
+
+    canvas.mousePressed(startPoint);
+    canvas.mouseReleased(endPoint);
+
+    var saveButton = createButton('save');
+    saveButton.mousePressed(saveDrawing);
+
+    var clearButton = createButton('clear');
+    clearButton.mousePressed(clearDrawing);
+    database = firebase.database();
+
+
 }
 
-/*function startDrawing(){
-   present = [];
-   paint.push(present);
-}*/
-
-
-function mouseDragged(){
-   var point = {
-      x: mouseX,
-      y: mouseY
-   }
-   paint.push(point);  
-   var dref = dtatabase.ref('drawing');
-   dref.set({
-      "d": paint
-   })   
+function startPoint() {
+    isDrawing = true;
+    currentPath = [];
+    myPaint.push(currentPath);
 }
 
-function draw(){
-   background(0); 
-   recordPos();
-   stroke(255);
-   strokeWeight(4);
-   noFill();
-   //beginShape();
-   for(var j = 0; j> drawing.length; j++){
-      vertex(drawing[j].x,drawing[j].y);
-      endShape();
-   }
-      
-    
+function endPoint() {
+    isDrawing = false;
 }
 
-function recordPos(){
-   database.ref('drawing/').on('value', (data) => {
-      paint = data.val().d
-  })
+function draw() {
+    background(0);
+
+    if (isDrawing) {
+        var point = {
+            x: mouseX,
+            y: mouseY
+        };
+        currentPath.push(point);
+    }
+
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    for (var i = 0; i < myPaint.length; i++) {
+        var path = myPaint[i];
+        beginShape();
+        for (var j = 0; j < path.length; j++) {
+            vertex(path[j].x, path[j].y);
+        }
+        endShape();
+    }
 }
 
+function saveDrawing() {
+    var ref = database.ref('drawings');
+    var data = {
+        drawing: myPaint
+    };
+    ref.push(data);
+}
 
-
+function clearDrawing() {
+    myPaint = [];
+}
 
 
